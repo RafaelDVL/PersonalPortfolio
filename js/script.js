8902aae76785bbf9a0f1eab712637dca07bbb6c4
RafaelDvl <rafaelrodrigues.dsg3d@gmail.com>
2022-11-03 15:54:49 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+
+var typed =  new Typed(".typing", {
+    strings:["Web Designer.", "Developer Java Full Stack.", "Lover Tecnology.", "3D Printer."],
+    typeSpeed:80,
+    BackSpeed:40,
+    loop:true
+})
